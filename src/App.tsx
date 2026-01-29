cc9f968853478c9f689ee885f40376907bb72eb3
PFG-Samson <adeyemoye@proforcegalaxies.com>
2026-01-29 11:51:10 +0100

    I've updated src/App.tsx  to include the v7 future flags for React Router. This will silence the deprecation warnings in your console.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <SettingsProvider>
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
